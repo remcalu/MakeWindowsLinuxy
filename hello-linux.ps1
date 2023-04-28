1880de8eb659ced9162ffd8d426d3f72293a934e
Remus Calugarescu <remcalu@outlook.com>
2023-04-28 14:45:46 -0400

    Further improve scripts

--- hello-linux.ps1
+++ hello-linux.ps1
@@ -16,8 +16,30 @@ choco install gsudo -y
 
 # Set up zsh
 Write-Host "Setting up zsh..." -ForegroundColor Green
-Copy-Item -Path "zsh-files\*" -Destination "C:\Program Files\Git" -Force
 
+########################################################################
+
+# Moving files from the etc directory
+xcopy "zsh-files\etc\zsh" "C:\Program Files\Git\etc" /I
+
+# Moving files from the usr\bin directory
+xcopy "zsh-files\usr\bin\msys-zsh-5.9.dll" "C:\Program Files\Git\usr\bin" /I
+xcopy "zsh-files\usr\bin\zsh-5.9.exe" "C:\Program Files\Git\usr\bin" /I
+xcopy "zsh-files\usr\bin\zsh.exe" "C:\Program Files\Git\usr\bin" /I
+
+# Moving files from the usr\lib directory
+xcopy "zsh-files\usr\lib\zsh" "C:\Program Files\Git\usr\lib" /I
+
+# Moving files from the usr\share\licenses
+xcopy "zsh-files\usr\share\licenses\zsh" "C:\Program Files\Git\usr\share\licenses" /I
+
+# Moving files from the usr\man
+xcopy "zsh-files\usr\share\man" "C:\Program Files\Git\usr\share" /I
+
+# Moving files from the usr\share
+xcopy "zsh-files\usr\share\zsh" "C:\Program Files\Git\usr\share" /I
+
+########################################################################
 # Launch git bash and run more zsh set up commands
 Write-Host "Launching git bash and setting up zsh..." -ForegroundColor Green
 & 'C:\Program Files\Git\bin\bash.exe' zsh-setup.sh

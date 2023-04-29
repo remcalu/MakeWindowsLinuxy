d5535751f14c5994df1ab8fe332b67f0a1189d04
Remus Calugarescu <59599955+remcalu@users.noreply.github.com>
2023-04-29 19:42:15 -0400

    Update setup-zsh.sh

--- scripts-bash/setup-zsh.sh
+++ scripts-bash/setup-zsh.sh
@@ -2,4 +2,4 @@
 touch ~/.zshrc
 
 # Setting up oh-my-zsh for a nicer looking shell
-sh -c "RUNZSH=no $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "RUNZSH=no $(curl -fsSLk https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

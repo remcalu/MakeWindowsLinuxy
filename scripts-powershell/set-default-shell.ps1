924239239d66ee2147a5a0154f17efb7af4cd779
Remus Calugarescu <remcalu@outlook.com>
2023-04-29 17:43:00 -0400

    Fix 1 more thing

--- scripts-powershell/set-default-shell.ps1
+++ scripts-powershell/set-default-shell.ps1
@@ -1,4 +1,4 @@
 # Setting git bash as default bash
 Write-Host "Setting git bash as default bash..." -ForegroundColor Green
 New-Item -ItemType File -Path $PROFILE -Force
-Add-Content $PROFILE "`n& 'C:\Program Files\Git\bin\bash.exe'`n"
+Add-Content $PROFILE "`n& 'C:\Program Files\Git\usr\bin\zsh.exe'`n"

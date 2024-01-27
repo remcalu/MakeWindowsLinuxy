2f9db765fde6e2775437467f4ce5606d390c00d8
Remus Calugarescu <59599955+remcalu@users.noreply.github.com>
2024-01-27 16:38:58 -0500

    Update install-python3.ps1

--- scripts-powershell/choco-packages/install-python3.ps1
+++ scripts-powershell/choco-packages/install-python3.ps1
@@ -1,3 +1,3 @@
 # Install python3 using choco package manager
 Write-Host "Install python3 using choco package manager..." -ForegroundColor Green
-choco install python3 -y
+choco install python3 --version 3.11.7 -y
